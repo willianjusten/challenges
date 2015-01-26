5a7b3c15cbeed052bb40cf12f78b805619c5b815
willianjusten <willianjustenqui@gmail.com>
2015-01-26 12:37:58 -0200

    Add missingNumber.js exercise

---
+++ missingNumber.js
@@ -0,0 +1,21 @@
+/**
+ Given an array containing all numbers from 1 to N with the exception of one print the missing number to the standard output.
+Example input:
+array: 5 4 1 2
+Example output:
+3
+ */
+
+
+function find_missing_number(v) {
+    var tam = v.length;
+    var i;
+
+    for (i = 1; i <= tam; i++) {
+        // verifica se não existe dentro do array
+        if(v.indexOf(i) === -1)
+            console.log(i);
+    };
+};
+
+find_missing_number([5,4,1,2]);
